6301488a7ff7ab38360e82049166a7f15deac342
bocharsky-bw <bocharsky.bw@gmail.com>
2020-01-20 23:49:05 +0200

    Add required TwigBundle and FrameworkBundle

--- src/BW/ActiveMenuItemBundle/Twig/BWExtension.php
+++ src/BW/ActiveMenuItemBundle/Twig/BWExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class BWExtension extends \Twig_Extension
 {
     /**
-     * @var \Symfony\Component\HttpFoundation\Request
+     * @var Request
      */
     protected $request;
 
